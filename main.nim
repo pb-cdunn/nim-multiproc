def9fe73bad4ae5400ae0dc6c793b1bf4bf4e4da
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-03-28 11:57:42 -0700

    newPool/Pool/Fork

--- main.nim
+++ main.nim
@@ -1,7 +1,10 @@
 # vim: sw=2 ts=2 sts=2 tw=80 et:
 import multiproc
 
+proc go() =
+  echo "hello"
 proc main() =
   echo "hi"
+  var pool = multiproc.newPool(1)
 
 main()
--- multiproc.nim
+++ multiproc.nim
@@ -1,2 +1,10 @@
 # vim: sw=2 ts=2 sts=2 tw=80 et:
 
+type
+  Fork = ref object
+  Pool = ref object
+    forks: seq[Fork]
+
+proc newPool*(n: int): Pool =
+  new(result)
+  newSeq(result.forks, n)
