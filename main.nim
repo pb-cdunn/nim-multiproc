c6183a4b38883b7a8fa4a0f5499522b8f94b6933
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-03-28 12:14:04 -0700

    Pass arg

--- main.nim
+++ main.nim
@@ -1,11 +1,11 @@
 # vim: sw=2 ts=2 sts=2 tw=80 et:
 import multiproc
 
-proc go() =
-  echo "hello"
+proc go(x: int) =
+  echo "hello:", x
 proc main() =
   echo "hi"
   var pool = multiproc.newPool(1)
-  pool.apply_async(go)
+  pool.apply_async(go, 1)
 
 main()
--- multiproc.nim
+++ multiproc.nim
@@ -8,6 +8,6 @@ type
 proc newPool*(n: int): Pool =
   new(result)
   newSeq(result.forks, n)
-proc apply_async*(pool: Pool, f: proc()) =
+proc apply_async*[TArg](pool: Pool, f: proc(arg: TArg), arg: TArg) =
   echo "no way"
-  f()
+  f(arg)
