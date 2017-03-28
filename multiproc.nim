da5f4e49ca02a72e2587a44ddc02b3dee6d48262
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-03-28 12:31:55 -0700

    msgpack/unpack

--- multiproc.nim
+++ multiproc.nim
@@ -1,4 +1,6 @@
 # vim: sw=2 ts=2 sts=2 tw=80 et:
+from "msgpack4nim/msgpack4nim" as msgpack import nil
+from streams import nil
 
 type
   Fork = ref object
@@ -9,5 +11,12 @@ proc newPool*(n: int): Pool =
   new(result)
   newSeq(result.forks, n)
 proc apply_async*[TArg](pool: Pool, f: proc(arg: TArg), arg: TArg) =
-  echo "no way"
-  f(arg)
+  var s = streams.newStringStream()
+  echo "ser..."
+  msgpack.pack(s, arg)
+
+  streams.setPosition(s, 0)
+  echo "deser..."
+  var argP: TArg
+  msgpack.unpack(s, argP)
+  f(argP)
