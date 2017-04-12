58b4216b10dbcf79392acad1c272475b2486cc71
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-04-12 13:42:08 -0700

    Return result

--- main.nim
+++ main.nim
@@ -1,11 +1,14 @@
 # vim: sw=2 ts=2 sts=2 tw=80 et:
 import multiproc
+import asyncdispatch
 
-proc go(x: int) =
+proc go(x: int): int =
   echo "hello:", x
+  return 42
 proc main() =
   echo "hi"
   var pool = multiproc.newPool(1)
-  pool.apply_async(go, 1)
+  var result = pool.apply_async(go, 1)
+  echo "result:", $result
 
 main()
--- multiproc.nim
+++ multiproc.nim
@@ -2,9 +2,17 @@
 from "msgpack4nim/msgpack4nim" as msgpack import nil
 from streams import nil
 from posix import nil
+from os import nil
 
 # http://beej.us/guide/bgipc/output/html/singlepage/bgipc.html
 posix.signal(posix.SIGCHLD, posix.SIGIGN) # Do not wait() for childs.
+#var sid = posix.setsid() # This could prevent us from hearing Ctrl-C, but makes
+#all sub-procs killable via 'kill -PID'
+
+posix.onSignal(posix.SIGINT):
+  echo "Received SIGINT" & $posix.getpid()
+  discard posix.kill(posix.getpid(), posix.SIGINT)
+  system.quit(system.QuitFailure)
 
 type
   Fork = ref object
@@ -16,7 +24,9 @@ proc err(msg: string) =
     raise newException(OSError, $posix.strerror(posix.errno) & ":" & msg)
 proc prepareChild(fork: var Fork) =
   # We are in the child.
-  posix.signal(posix.SIGINT, posix.SIG_DFL)
+  #posix.signal(posix.SIGINT, posix.SIG_DFL)
+  while true:
+    os.sleep(1000)
   system.quit(system.QuitSuccess)
 proc newFork(): Fork =
   new(result)
@@ -27,6 +37,9 @@ proc newFork(): Fork =
   elif pid > 0:
     echo "Parent forked:", pid
     result.pid = pid
+    #while true:
+    #  os.sleep(1000)
+    #system.quit(system.QuitSuccess)
   else:
     err("fork() failed:" & $pid)
 proc newPool*(n: int): Pool =
@@ -34,7 +47,7 @@ proc newPool*(n: int): Pool =
   newSeq(result.forks, n)
   for i in 0..<n:
     result.forks[i] = newFork()
-proc apply_async*[TArg](pool: Pool, f: proc(arg: TArg), arg: TArg) =
+proc apply_async*[TArg,TResult](pool: Pool, f: proc(arg: TArg): TResult, arg: TArg): TResult =
   var s = streams.newStringStream()
   echo "ser..."
   msgpack.pack(s, arg)
@@ -43,4 +56,4 @@ proc apply_async*[TArg](pool: Pool, f: proc(arg: TArg), arg: TArg) =
   echo "deser..."
   var argP: TArg
   msgpack.unpack(s, argP)
-  f(argP)
+  return f(argP)
