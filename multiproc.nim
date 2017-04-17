cd1b2d3faf3fe8cb2fb49901ee0ee3723ee4a2f1
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-04-17 10:13:17 -0700

    Close all unused fds in child; end comm gracefully
    * Keep a list of all fds so each new child fork can close everything.
    * End communication by sending and then receiving a 0 msg_len in parent;
      child replies and quits.
    
    This does not solve the problem.

--- multiproc.nim
+++ multiproc.nim
@@ -44,7 +44,7 @@ proc close(fd: cint) =
   if ret != 0:
     os.raiseOsError(os.osLastError())
 
-proc prepareChild(fork: Fork) =
+proc prepareChild(fork: Fork, other_fds: seq[cint]) =
   # We are in the child.
   #posix.signal(posix.SIGINT, posix.SIG_DFL)
   #while true:
@@ -58,9 +58,12 @@ proc prepareChild(fork: Fork) =
     #discard posix.kill(posix.getpid(), posix.SIGINT)
     system.quit(system.QuitSuccess)
   echo "In prepareChild()"
-  echo " -Closing fds:", fork.pipe_child2parent_rw[0], " & ", fork.pipe_parent2child_rw[1]
-  close(fork.pipe_child2parent_rw[0]) # read end
-  close(fork.pipe_parent2child_rw[1]) # write end
+  #echo " -Closing fds:", fork.pipe_child2parent_rw[0], " & ", fork.pipe_parent2child_rw[1]
+  #close(fork.pipe_child2parent_rw[0]) # read end
+  #close(fork.pipe_parent2child_rw[1]) # write end
+  echo " -Closing fds:", repr(other_fds)
+  for i in 0..<len(other_fds):
+    close(i.cint)
   prepareForDispatch(fork.pipe_child2parent_rw[1])
   prepareForDispatch(fork.pipe_parent2child_rw[0])
 
@@ -71,6 +74,14 @@ proc prepareParent(fork: Fork) =
   close(fork.pipe_child2parent_rw[1]) # write end
   prepareForDispatch(fork.pipe_parent2child_rw[1])
   prepareForDispatch(fork.pipe_child2parent_rw[0])
+
+proc finishChild(fork: Fork) =
+  #echo "unregistering in Child..." # There is no point, but maybe we could try anyway?
+  asyncdispatch.unregister(fork.pipe_child2parent_rw[1].AsyncFD)
+  asyncdispatch.unregister(fork.pipe_parent2child_rw[0].AsyncFD)
+  close(fork.pipe_child2parent_rw[1]) # write end
+  close(fork.pipe_parent2child_rw[0]) # read end
+  echo " -Closed and unregistered fds:", fork.pipe_parent2child_rw[0], " & ", fork.pipe_child2parent_rw[1]
 
 proc finishParent(fork: Fork) =
   echo "unregistering in Parent..."
@@ -151,6 +162,12 @@ proc runChild[TArg,TResult](fork: Fork, f: proc(arg: TArg): TResult) =
       msg.setLen(msg_len) # I think this avoids zeroing the string first.
       when debug:
         echo "child recving msg of msg_len=", msg_len
+      if msg_len == 0:
+        echo "Child sending zero back"
+        writeAll(fork.pipe_child2parent_rw[1], addr msg_len, 8)
+        echo "Shutting down child:", posix.getpid()
+        finishChild(fork)
+        system.quit(system.QuitFailure)
       waitFor readAll(fork.pipe_parent2child_rw[0], cstring(msg), msg_len)
     except OsError:
       echo "trapped exception in runChild():" & getCurrentExceptionMsg()
@@ -173,6 +190,14 @@ proc runChild[TArg,TResult](fork: Fork, f: proc(arg: TArg): TResult) =
     when debug:
       echo "child sending msg of msg_len=", msg_len
     writeAll(fork.pipe_child2parent_rw[1], cstring(msg), msg_len)
+
+proc sendZero(fork: Fork) =
+  # By sending zero (64-bit), we let the child know we done.
+  var msg_len: int = 0
+  echo "parent sendZero()"
+  writeAll(fork.pipe_parent2child_rw[1], addr msg_len, 8)
+  echo "parent recv zero"
+  waitFor readAll(fork.pipe_child2parent_rw[0], addr msg_len, 8)
 
 proc runParent*[TArg,TResult](fork: Fork, arg: TArg): Future[TResult] {.async.} =
   var msg_len: int
@@ -204,7 +229,8 @@ proc runParent*[TArg,TResult](fork: Fork, arg: TArg): Future[TResult] {.async.} 
   #return retFuture
   return call_result
 
-proc newRpcFork[TArg,TResult](f: proc(arg: TArg): TResult): Fork =
+proc newRpcFork[TArg,TResult](f: proc(arg: TArg): TResult, other_fds: var seq[cint]): Fork =
+  # We send other_fds so the child can close everything it does not need.
   new(result)
   block:
     let ret = posix.pipe(result.pipe_child2parent_rw)
@@ -212,14 +238,18 @@ proc newRpcFork[TArg,TResult](f: proc(arg: TArg): TResult): Fork =
   block:
     let ret = posix.pipe(result.pipe_parent2child_rw)
     assert ret == 0
+  other_fds.add(result.pipe_child2parent_rw[0])
+  other_fds.add(result.pipe_parent2child_rw[1])
   var pid = posix.fork()
   if pid == 0:
     when debug:
-      echo "In child with pid:", posix().getpid
-    prepareChild(result)
+      echo "In child with pid:", posix.getpid()
+    prepareChild(result, other_fds)
     runChild[TArg,TResult](result, f)
     system.quit(system.QuitFailure)
   elif pid > 0:
+    other_fds.add(result.pipe_child2parent_rw[1])
+    other_fds.add(result.pipe_parent2child_rw[0])
     when debug:
       echo "Parent forked child with pid:", pid
     result.pid = pid
@@ -229,9 +259,12 @@ proc newRpcFork[TArg,TResult](f: proc(arg: TArg): TResult): Fork =
 proc newRpcPool*[TArg,TResult](n: int, f: proc(arg: TArg): TResult): Pool =
   new(result)
   newSeq(result.forks, n)
+  var other_fds: seq[cint]
+  newSeq(other_fds, 0)
   for i in 0..<n:
     echo "i=", i
-    result.forks[i] = newRpcFork[TArg,TResult](f)
+    result.forks[i] = newRpcFork[TArg,TResult](f, other_fds)
+  echo "other_fds:", repr(other_fds)
   for i in 0..<n:
     let fork = result.forks[i]
     echo "prepareParent for i=", i
@@ -241,6 +274,8 @@ proc closePool*(pool: Pool) =
   # Remember to call this (in a finally block) or you will have dangling
   # children in some errors.
   echo "closing Pool"
+  for i in 0..<len(pool.forks):
+    sendZero(pool.forks[i])
   for i in 0..<len(pool.forks):
     echo "terminating newRpcFork for i=", i
     discard posix.kill(pool.forks[i].pid, posix.SIGTERM)
