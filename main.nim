ac617d332b7c32b6ddf6c49ff31748cc20f452b2
Christopher Dunn <cdunn2001@gmail.com>
2017-05-07 22:34:40 -0500

    Working

--- main.nim
+++ main.nim
@@ -3,12 +3,35 @@ from asyncdispatch import nil
 from strutils import nil
 import multiproc
 import asyncdispatch
+from posix import nil
+
+const resettable = false
 
 proc square(x: int): int =
   echo "hello:", x
   return x*x
 
+proc example0() =
+  var fds: array[0..1, cint]
+  let ret = posix.pipe(fds)
+  echo "b4"
+  asyncdispatch.register(fds[0].AsyncFD)
+  proc cb(afd: asyncdispatch.AsyncFD): bool =
+    echo "Hellooo"
+  asyncdispatch.addRead(fds[0].AsyncFD, cb)
+  echo "polling..."
+  asyncdispatch.poll()
+  echo "polled"
+  asyncdispatch.unregister(fds[0].AsyncFD)
+  #asyncdispatch.poll()
+  echo "aft"
+  when resettable:
+    asyncdispatch.setGlobalDispatcher(nil)
+  echo "aft2"
 proc example1() =
+  #asyncdispatch.setGlobalDispatcher(asyncdispatch.newDispatcher())
+  when resettable:
+    asyncdispatch.setGlobalDispatcher(nil)
   var rpool = multiproc.newRpcPool[int,int](1, square)
   try:
     var call_result = multiproc.runParent[int,int](rpool.forks[0], 7)
@@ -29,6 +52,10 @@ proc example2() =
     rpool.closePool()
 
 proc example3() =
+  echo "example 3"
+  when resettable:
+    #asyncdispatch.setGlobalDispatcher(asyncdispatch.newDispatcher())
+    asyncdispatch.setGlobalDispatcher(nil)
   const n = 2
   var rpool = multiproc.newRpcPool[int,string](n, big)
   var results: seq[asyncdispatch.Future[string]]
@@ -37,7 +64,8 @@ proc example3() =
     for i in 0..<n:
       var call_result = multiproc.runParent[int,string](rpool.forks[i], 10_000_000)
       results[i] = call_result
-    for i in 0..<n:
+    for j in 0..<n:
+      let i = n - 1 - j
       echo "waitFor result[", i, "]:", results[i].finished
       let length = len(asyncdispatch.waitFor results[i])
       echo "Final from child " & $i & ": len=" & $length
@@ -52,10 +80,11 @@ proc example4() =
 
 proc main() =
   echo "hi"
+  example0()
   example1()
   #example2()
   example3()
-  example4()
+  #example4()
   echo "bye"
 
 main()
--- multiproc.nim
+++ multiproc.nim
@@ -22,8 +22,27 @@ type
   Pool = ref object
     forks*: seq[Fork]
 
+var gpid: int
+var gpids: seq[int]
+newSeq(gpids, 0)
+
+proc log(msg: string) =
+  stderr.write("[" & $gpid & "]" & msg & "\L")
 proc err(msg: string) =
     raise newException(OSError, $posix.strerror(posix.errno) & ":" & msg)
+#posix.onSignal(posix.SIGINT):
+#  log("Received SIGINT in " & $posix.getpid())
+#  for i in 0..<len(gpids):
+#    let kpid = gpids[i]
+#    log($i & ":" & $kpid & "Alive?" & $posix.kill(kpid, 0))
+#    discard posix.kill(kpid, posix.SIGINT)
+#    discard posix.kill(kpid, posix.SIGKILL)
+#    #os.sleep(100)
+#    log($i & ":" & $kpid & "Alive?" & $posix.kill(kpid, 0))
+#  posix.signal(posix.SIGINT, posix.SIG_DFL)
+#  discard posix.kill(gpid, posix.SIGINT)
+#  #system.quit(system.QuitFailure)
+
 
 proc setNonBlocking(fd: cint) {.inline.} =
   var x = posix.fcntl(fd, posix.F_GETFL, 0)
@@ -35,61 +54,71 @@ proc setNonBlocking(fd: cint) {.inline.} =
       os.raiseOSError(os.osLastError())
 
 proc prepareForDispatch(fd: cint) {.inline.} =
-  echo "prepareForDispatch (register and setNonBlocking) fd:", fd
+  log("prepareForDispatch (register and setNonBlocking) fd:" & $fd)
   setNonBlocking(fd)
   asyncdispatch.register(fd.AsyncFD)
 
 proc close(fd: cint) =
   let ret = posix.close(fd)
+  when debug:
+    log(   "Closed:" & $fd)
   if ret != 0:
     os.raiseOsError(os.osLastError())
 
 proc prepareChild(fork: Fork, other_fds: seq[cint]) =
   # We are in the child.
-  #posix.signal(posix.SIGINT, posix.SIG_DFL)
+  when debug:
+    posix.signal(posix.SIGINT, posix.SIG_DFL)
   #while true:
   #  os.sleep(1000)
   #posix.onSignal(posix.SIGINT):
-  #  echo "Received SIGINT" & $posix.getpid()
+  #  log("Received SIGINT" & $posix.getpid())
   #  discard posix.kill(posix.getpid(), posix.SIGINT)
   #  system.quit(system.QuitFailure)
   posix.onSignal(posix.SIGTERM):
-    echo "Received SIGTERM" & $posix.getpid()
+    log("Received SIGTERM" & $posix.getpid())
     #discard posix.kill(posix.getpid(), posix.SIGINT)
     system.quit(system.QuitSuccess)
-  echo "In prepareChild()"
-  #echo " -Closing fds:", fork.pipe_child2parent_rw[0], " & ", fork.pipe_parent2child_rw[1]
+  when debug:
+    log("In prepareChild()")
+  #log(" -Closing fds:" & $fork.pipe_child2parent_rw[0] & " & " & $fork.pipe_parent2child_rw[1])
   #close(fork.pipe_child2parent_rw[0]) # read end
   #close(fork.pipe_parent2child_rw[1]) # write end
-  echo " -Closing fds:", repr(other_fds)
+  when debug:
+    log(" -Closing fds:" & $repr(other_fds))
   for i in 0..<len(other_fds):
-    close(i.cint)
-  prepareForDispatch(fork.pipe_child2parent_rw[1])
+    close(other_fds[i].cint)
+  #prepareForDispatch(fork.pipe_child2parent_rw[1])
   prepareForDispatch(fork.pipe_parent2child_rw[0])
 
 proc prepareParent(fork: Fork) =
-  echo "In prepareParent()"
-  echo " Closing fds:", fork.pipe_child2parent_rw[1], " & ", fork.pipe_parent2child_rw[0]
+  when debug:
+    log("In prepareParent()")
+    log(" Closing fds:" & $fork.pipe_child2parent_rw[1] & " & " & $fork.pipe_parent2child_rw[0])
   close(fork.pipe_parent2child_rw[0]) # read end
   close(fork.pipe_child2parent_rw[1]) # write end
-  prepareForDispatch(fork.pipe_parent2child_rw[1])
+  #prepareForDispatch(fork.pipe_parent2child_rw[1])
   prepareForDispatch(fork.pipe_child2parent_rw[0])
+  gpids.add(fork.pid)
 
 proc finishChild(fork: Fork) =
-  #echo "unregistering in Child..." # There is no point, but maybe we could try anyway?
-  asyncdispatch.unregister(fork.pipe_child2parent_rw[1].AsyncFD)
+  #log("unregistering in Child...") # There is no point, but maybe we could try anyway?
+  #asyncdispatch.unregister(fork.pipe_child2parent_rw[1].AsyncFD)
   asyncdispatch.unregister(fork.pipe_parent2child_rw[0].AsyncFD)
   close(fork.pipe_child2parent_rw[1]) # write end
   close(fork.pipe_parent2child_rw[0]) # read end
-  echo " -Closed and unregistered fds:", fork.pipe_parent2child_rw[0], " & ", fork.pipe_child2parent_rw[1]
+  when debug:
+    log(" -Closed and unregistered fds:" & $fork.pipe_parent2child_rw[0] & " & " & $fork.pipe_child2parent_rw[1])
 
 proc finishParent(fork: Fork) =
-  echo "unregistering in Parent..."
-  asyncdispatch.unregister(fork.pipe_parent2child_rw[1].AsyncFD)
+  when debug:
+    log("unregistering in Parent...")
+  #asyncdispatch.unregister(fork.pipe_parent2child_rw[1].AsyncFD)
   asyncdispatch.unregister(fork.pipe_child2parent_rw[0].AsyncFD)
   close(fork.pipe_parent2child_rw[1]) # write end
   close(fork.pipe_child2parent_rw[0]) # read end
-  echo " Closed and unregistered fds:", fork.pipe_child2parent_rw[0], " & ", fork.pipe_parent2child_rw[1]
+  when debug:
+    log(" Closed and unregistered fds:" & $fork.pipe_child2parent_rw[0] & " & " & $fork.pipe_parent2child_rw[1])
 
 proc readAll(fd: cint, start: pointer, nbytes: int): Future[void] =
   var retFuture = asyncdispatch.newFuture[void]("multiproc.readAll")
@@ -101,19 +130,19 @@ proc readAll(fd: cint, start: pointer, nbytes: int): Future[void] =
     result = true # Tell dispatcher to stop calling this callback.
 
     while bytesSoFar < nbytes:
-      var ret: int = posix.read(afd.cint, cast[pointer](current), nbytes)
+      var ret: int = posix.read(afd.cint, cast[pointer](current), nbytes-bytesSoFar)
       if ret > 0:
         bytesSoFar += ret
         current = current +% ret
-        #echo "Read " & $ret & " bytes. togo=" & $(nbytes-bytesSoFar)
+        #log("Read " & $ret & " bytes. togo=" & $(nbytes-bytesSoFar))
       elif ret == 0:
         #os.raiseOsError(os.osLastError(), "In readAll(), fd was closed.")
         when debug:
-          echo "Read 0 bytes. Is that OK?" # TODO(CD): Remove.
+          log("Read 0 bytes. Is that OK?") # TODO(CD): Remove.
         return
       else:
         let err = os.osLastError()
-        #echo "Error:" & os.osErrorMsg(err) & ":" & $err.int & ", result=" & $result & ", bytestogo=" & $(nbytes-bytesSoFar)
+        #log("Error:" & os.osErrorMsg(err) & ":" & $err.int & ", result=" & $result & ", bytestogo=" & $(nbytes-bytesSoFar))
         if err.int32 != posix.EAGAIN:
           asyncdispatch.fail(retFuture, newException(OSError, "In readAll(), ret<0: " & os.osErrorMsg(err)))
           #result = true # We still want this callback to be called.
@@ -121,11 +150,16 @@ proc readAll(fd: cint, start: pointer, nbytes: int): Future[void] =
           result = false # We still want this callback to be called.
         return
     when debug:
-      echo "Completing retFuture, bytesSoFar=" & $bytesSoFar
+      log("Completing retFuture " & $retFuture.id & ", bytesSoFar=" & $bytesSoFar)
+      assert result
     asyncdispatch.complete(retFuture)
 
   if not cb(fd.AsyncFD):
+    when debug:
+      log("cb(" & $fd & ") returned false. Adding cb.")
     addRead(fd.AsyncFD, cb)
+  when debug:
+    log("Returning retFuture from readAll() - finished?" & $retFuture.id & $retFuture.finished)
   return retFuture
 
 proc writeAll(fd: cint, start: pointer, nbytes: int) =
@@ -135,6 +169,8 @@ proc writeAll(fd: cint, start: pointer, nbytes: int) =
   var bytesSoFar: int = 0
   var current: ByteAddress = cast[ByteAddress](start)
   while bytesSoFar < nbytes:
+    when debug:
+      log("togo:" & $(nbytes-bytesSoFar))
     ret = posix.write(fd, cast[pointer](current), nbytes-bytesSoFar)
     if ret > 0:
       bytesSoFar += ret
@@ -149,54 +185,58 @@ proc writeAll(fd: cint, start: pointer, nbytes: int) =
 proc runChild[TArg,TResult](fork: Fork, f: proc(arg: TArg): TResult) =
   # We are in the child.
   #posix.signal(posix.SIGINT, posix.SIG_DFL)
-  var ret: int
+  #var ret: int
   var msg_len: int
   var msg = newString(0)
   while true:
     # recv
     try:
       when debug:
-        echo "child reading msg_len"
+        log("child reading msg_len on " & $fork.pipe_parent2child_rw[0])
       waitFor readAll(fork.pipe_parent2child_rw[0], addr msg_len, 8)
       assert sizeof(msg_len) == 8
       msg.setLen(msg_len) # I think this avoids zeroing the string first.
       when debug:
-        echo "child recving msg of msg_len=", msg_len
+        log("child recving msg of msg_len=" & $msg_len)
       if msg_len == 0:
-        echo "Child sending zero back"
+        log("Child sending zero back")
         writeAll(fork.pipe_child2parent_rw[1], addr msg_len, 8)
-        echo "Shutting down child:", posix.getpid()
+        log("Shutting down child:" & $posix.getpid())
         finishChild(fork)
         system.quit(system.QuitFailure)
       waitFor readAll(fork.pipe_parent2child_rw[0], cstring(msg), msg_len)
+      when debug:
+        log("child read full msg on " & $fork.pipe_parent2child_rw[0])
     except OsError:
-      echo "trapped exception in runChild():" & getCurrentExceptionMsg()
+      log("trapped exception in runChild():" & getCurrentExceptionMsg())
       continue
     except:
-      echo "trapped unknown exception in runChild()"
+      log("trapped unknown exception in runChild()")
       raise
     var call_arg: TArg
     msgpack.unpack(msg, call_arg)
     when debug:
-      echo "child call_arg=", call_arg
+      log("child call_arg=" & $call_arg)
     var call_result = f(call_arg)
 
     # send
     msg = msgpack.pack(call_result)
     msg_len = len(msg)
     when debug:
-      echo "child sending msg_len"
+      log("child sending msg_len on" & $fork.pipe_child2parent_rw[1])
     writeAll(fork.pipe_child2parent_rw[1], addr msg_len, 8)
     when debug:
-      echo "child sending msg of msg_len=", msg_len
+      log("child sending msg of msg_len=" & $msg_len)
     writeAll(fork.pipe_child2parent_rw[1], cstring(msg), msg_len)
 
 proc sendZero(fork: Fork) =
   # By sending zero (64-bit), we let the child know we done.
   var msg_len: int = 0
-  echo "parent sendZero()"
+  when debug:
+    log("parent sendZero()")
   writeAll(fork.pipe_parent2child_rw[1], addr msg_len, 8)
-  echo "parent recv zero"
+  when debug:
+    log("parent recv zero")
   waitFor readAll(fork.pipe_child2parent_rw[0], addr msg_len, 8)
 
 proc runParent*[TArg,TResult](fork: Fork, arg: TArg): Future[TResult] {.async.} =
@@ -207,27 +247,29 @@ proc runParent*[TArg,TResult](fork: Fork, arg: TArg): Future[TResult] {.async.} 
   msg = msgpack.pack(arg)
   msg_len = len(msg)
   when debug:
-    echo "parent sending msg_len"
+    log("parent sending msg_len on " & $fork.pipe_parent2child_rw[1])
   writeAll(fork.pipe_parent2child_rw[1], addr msg_len, 8)
   when debug:
-    echo "parent sending msg of msg_len=", msg_len
+    log("parent sending msg of msg_len=" & $msg_len)
   writeAll(fork.pipe_parent2child_rw[1], cstring(msg), msg_len)
 
   # recv
   when debug:
-    echo "parent reading msg_len"
+    log("parent reading msg_len on " & $fork.pipe_child2parent_rw[0])
   await readAll(fork.pipe_child2parent_rw[0], addr msg_len, 8)
   assert sizeof(msg_len) == 8
   msg.setLen(msg_len) # I think this avoids zeroing the string first.
   when debug:
-    echo "parent recving msg of msg_len=", msg_len
+    log("parent recving msg of msg_len=" & $msg_len)
   await readAll(fork.pipe_child2parent_rw[0], cstring(msg), msg_len)
-  var call_result: TResult
-  msgpack.unpack(msg, call_result)
-  #msgpack.unpack(msg, result)
+  log("parent finished both await readAll()")
+  #var call_result: TResult
+  #msgpack.unpack(msg, call_result)
+  msgpack.unpack(msg, result)
   #asyncdispatch.complete(retFuture, call_result)
   #return retFuture
-  return call_result
+  log("parent returning call_result")
+  #return call_result
 
 proc newRpcFork[TArg,TResult](f: proc(arg: TArg): TResult, other_fds: var seq[cint]): Fork =
   # We send other_fds so the child can close everything it does not need.
@@ -241,9 +283,10 @@ proc newRpcFork[TArg,TResult](f: proc(arg: TArg): TResult, other_fds: var seq[ci
   other_fds.add(result.pipe_child2parent_rw[0])
   other_fds.add(result.pipe_parent2child_rw[1])
   var pid = posix.fork()
+  gpid = posix.getpid().int
   if pid == 0:
     when debug:
-      echo "In child with pid:", posix.getpid()
+      log("In child with pid:" & $posix.getpid())
     prepareChild(result, other_fds)
     runChild[TArg,TResult](result, f)
     system.quit(system.QuitFailure)
@@ -251,7 +294,7 @@ proc newRpcFork[TArg,TResult](f: proc(arg: TArg): TResult, other_fds: var seq[ci
     other_fds.add(result.pipe_child2parent_rw[1])
     other_fds.add(result.pipe_parent2child_rw[0])
     when debug:
-      echo "Parent forked child with pid:", pid
+      log("Parent forked child with pid:" & $pid)
     result.pid = pid
   else:
     err("fork() failed:" & $pid)
@@ -262,44 +305,49 @@ proc newRpcPool*[TArg,TResult](n: int, f: proc(arg: TArg): TResult): Pool =
   var other_fds: seq[cint]
   newSeq(other_fds, 0)
   for i in 0..<n:
-    echo "i=", i
+    log("i=" & $i)
     result.forks[i] = newRpcFork[TArg,TResult](f, other_fds)
-  echo "other_fds:", repr(other_fds)
+  log("other_fds:" & repr(other_fds))
   for i in 0..<n:
     let fork = result.forks[i]
-    echo "prepareParent for i=", i
+    log("prepareParent for i=" & $i)
     prepareParent(fork)
 
 proc closePool*(pool: Pool) =
   # Remember to call this (in a finally block) or you will have dangling
   # children in some errors.
-  echo "closing Pool"
+  log("closing Pool")
   for i in 0..<len(pool.forks):
     sendZero(pool.forks[i])
   for i in 0..<len(pool.forks):
-    echo "terminating newRpcFork for i=", i
-    discard posix.kill(posix.Pid(pool.forks[i].pid), posix.SIGTERM)
+    log("terminating newRpcFork for i=" & $i)
+    let kpid = posix.Pid(pool.forks[i].pid)
+    while posix.kill(kpid, 0) != 0:
+      log("Waiting for " & $kpid & " to quit...")
+      os.sleep(500)
+    discard posix.kill(kpid, posix.SIGTERM)
   for i in 0..<len(pool.forks):
-    echo "killing newRpcFork for i=", i
-    discard posix.kill(posix.Pid(pool.forks[i].pid), posix.SIGKILL)
+    let kpid = posix.Pid(pool.forks[i].pid)
+    log("killing newRpcFork for i=" & $i)
+    discard posix.kill(kpid, posix.SIGKILL)
   for i in 0..<len(pool.forks):
-    echo "finishing newRpcFork for i=", i
+    log("finishing newRpcFork for i=" & $i)
     finishParent(pool.forks[i])
   while asyncdispatch.hasPendingOperations():
-    echo "Still pending..."
+    log("Still pending...")
     try:
       asyncdispatch.poll(500)
     except ValueError:
       err(getCurrentExceptionMsg())
-  echo "Finished pending..."
+  log("No pending operations...")
 
 proc apply_async*[TArg,TResult](pool: Pool, f: proc(arg: TArg): TResult, arg: TArg): TResult =
   var s = streams.newStringStream()
-  echo "ser..."
+  log("ser...")
   msgpack.pack(s, arg)
 
   streams.setPosition(s, 0)
-  echo "deser..."
+  log("deser...")
   var argP: TArg
   msgpack.unpack(s, argP)
   return f(argP)
